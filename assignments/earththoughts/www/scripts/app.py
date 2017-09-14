56b9e74b8ba60a14dcf9ebf14d1a30c26344af77
cwcook0414 <c.w.cook@live.com>
2017-09-14 00:37:38 -0500

    Finished Ass. 4

--- assignments/earththoughts/www/scripts/app.py
+++ assignments/earththoughts/www/scripts/app.py
@@ -3,7 +3,9 @@ from flask import request
 from flask_cors import CORS
 import json
 from flask import jsonify
+from flask import *
 import random
+import sys,os
 
 
 app = Flask(__name__)
@@ -68,147 +70,7 @@ def get_thought():
 
 @app.route('/home')
 def home():
-    return """
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-    <title>Porn Thoughts</title>
-
-    <!-- Bootstrap core CSS -->
-    <script
-    src="https://code.jquery.com/jquery-3.2.1.min.js"
-    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="jumbotron-narrow.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-<style>
-    #main_image {
-        background-image: url("https://i.redd.it/fsipwrv7fgkz.jpg");
-        background-color: #17234E;
-        margin-bottom: 0;`enter code here`
-        min-height: 50%;
-        background-repeat: no-repeat;
-        background-position: center;
-        -webkit-background-size: cover;
-        background-size: cover;
-    }
-</style>
-<style>
-    #main_text {
-        color: white;
-        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-    }
-</style>
-
-</head>
-
-<body>
-
-    <div class="container">
-    <div class="header clearfix">
-        <nav>
-        <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
-        </ul>
-        </nav>
-        <h3 class="text-muted">Earth Porn and Shower Thoughts</h3>
-    </div>
-
-    <div class="jumbotron" id="main_image">
-        <h1></h1>
-        <p class="lead" id="main_text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        
-    </div>
-    <p><a class="btn btn-lg btn-success" href=""http://localhost:5050/get_image""http://localhost:5050/get_thought"" role="button">New Porn and Thought?</a></p>
-    <div class="row marketing">
-        <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-    </div>
-
-    <footer class="footer">
-        <p>&copy; 2016 cwcook0414.</p>
-    </footer>
-
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-    <script>
-        $(function() {
-            console.log( "ready!" );
-            $.get( "http://localhost:5050/get_image", function( data ) {
-            
-                console.log(data.url)
-                $('#main_image').css('background-image','url('+data.url+')');
-            });
-        });
-
-    </script>
-    <script>
-        $(function() {
-            console.log( "ready!" );
-            $.get( "http://localhost:5050/get_thought", function( data ) {
-            
-                console.log(data.thought)
-                thought = data.thought
-                document.getElementById("main_text").textContent=thought;
-            });
-        });
-
-    </script>
-
-</body>
-</html>
-"""
-
+    return send_file('../index.html')
 
 
 if __name__ == '__main__':
